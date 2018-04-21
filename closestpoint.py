9d421a6262f02c6e08427b6a2d73c323284cb19f
stevegleds <stevegleds@gmail.com>
2018-04-21 07:12:51 +0100

    21 April 2018 Added code to parse sector csv to dictionary Next step is to use this to calculate nearest sector 20 April 2018

--- closestpoint.py
+++ closestpoint.py
@@ -1,9 +1,10 @@
 import numpy as np
 import csv
 
-RUN_FILE = 'sector_points_100.csv'  # this is a test file
+
+SECTOR_FILE = 'sector_points.csv'  # this is a test file
 RESULTS_FILE = 'results.csv'
-print('Data file used is: ', RUN_FILE)
+print('Data file used is: ', SECTOR_FILE)
 
 
 def parse(raw_file, delimiter):
@@ -23,7 +24,7 @@ def parse(raw_file, delimiter):
     parsed_data = []  # this list will store every row of data
     fields = csv_data.__next__()  # this will be the column headers; we can use .next() because csv_data is an iterator
     for row in csv_data:
-        if row[0] == "":  # there is no text in the field so no data to process
+        if row[1] == "":  # there is no text in the field so no data to process
             pass
         else:
             parsed_data.append(dict(zip(fields, row)))  # Creates a new dict item for each row with col header as key and stores in a list
@@ -37,12 +38,15 @@ def parse(raw_file, delimiter):
 def get_sector_points(sector_postcodes_full, source_type):
     sector_points = []
     if source_type == 'csv':
-        pass
+        for sector in sector_postcodes_full:
+            # print(sector['Latitude'], sector['Longitude'])
+            sector_point = (float(sector['Latitude']), float(sector['Longitude']))
+            sector_points.append(sector_point)
     else:
         for sector in sector_postcodes_full:
             sector_points.append((sector[1], sector[2]))
-        # print('Sector points are:', sector_points)
-        return sector_points
+    print('Sector points are:', sector_points, type(sector_points))
+    return sector_points
 
 
 def find_closest_sector(location, postcodes):
@@ -56,23 +60,28 @@ def find_closest_sector(location, postcodes):
     dist_2 = np.sum((postcodes - location) ** 2, axis=1)
     # print('dist:', dist_2)
     # print('argmin, dist:', np.argmin(dist_2), dist_2)
-    return np.argmin(dist_2), dist_2
-
+    print("Your points are:", postcodes[np.argmin(dist_2)], 'in position:', np.argmin(dist_2), type(np.argmin(dist_2)))
+    return int(np.argmin(dist_2))
 
 def main():
     # Call our parse function with required file an delimiter
-    sector_points_data = parse(RUN_FILE, ',')
+    sector_points_data = parse(SECTOR_FILE, ',')
     print(sector_points_data)
+    print(type(sector_points_data))
+    print('12th item is:', sector_points_data[12])
     print("keys are:", sector_points_data[0].keys())
     sector_postcodes_full = [('A', 1, 1), ('B', 2, 2), ('C', 3, 3)]
-    sector_points = get_sector_points(sector_postcodes_full, 'list')
-    # sector_points = get_sector_points(sector_postcodes_full, 'csv')
-    locations = [(-12.3, 11.5), (4, 7), (1, 3), (3, 1)]
+    # sector_points = get_sector_points(sector_postcodes_full, 'list')
+    sector_points = get_sector_points(sector_points_data, 'csv')
+    locations = [(57.11, -2.1), (57.1579, -2.09048), (57.13, -2.13), (3, 1)]
     postcodes_array = np.asarray(sector_points)
     for speedtest in locations:
         closest_sector = find_closest_sector(speedtest, postcodes_array)
         # print(closest_sector, 'Type:', type(closest_sector), 'element:', closest_sector[0], 'postcode:', sector_points[closest_sector[0]])
-        print('Your location is in Sector:', sector_postcodes_full[closest_sector[0]][0])
+        print('Index from array:', type(closest_sector))
+        print('Your location is in Sector:', sector_points_data[closest_sector]['Postcode'])
+        print('Your location:', speedtest)
+        print('Your sector location:', sector_points_data[closest_sector])
         print('NEXT')
 
 
