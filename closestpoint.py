ed6b3699a85ed0720db00df31a259723dd76f924
Steve Gledhill <stevegleds@gmail.com>
2018-04-23 10:48:28 +0100

    23 April 2018 Added timer so we can see how efficient it is Started using subset of data (1000 points) Took less than 1 second

--- closestpoint.py
+++ closestpoint.py
@@ -1,13 +1,13 @@
 import numpy as np
 import csv
-
+import time #todo : remove when code complete, only used to time the code for testing
 
 SECTOR_FILE = 'sector_points.csv'  # this is a test file
-RESULTS_FILE = 'results.csv'
+RESULTS_FILE = 'mappointsample1000.csv'
 print('Data file used is: ', SECTOR_FILE)
 
 
-def parse(raw_file, delimiter):
+def parse(raw_file, delimiter=','):
     """
     :param raw_file: probably csv file
     :param delimiter: specify delimiter #TODO add default arg : ','
@@ -35,54 +35,77 @@ def parse(raw_file, delimiter):
     opened_file.close()
     return parsed_data
 
-def get_sector_points(sector_postcodes_full, source_type):
-    sector_points = []
-    if source_type == 'csv':
-        for sector in sector_postcodes_full:
-            # print(sector['Latitude'], sector['Longitude'])
-            sector_point = (float(sector['Latitude']), float(sector['Longitude']))
-            sector_points.append(sector_point)
-    else:
-        for sector in sector_postcodes_full:
-            sector_points.append((sector[1], sector[2]))
-    print('Sector points are:', sector_points, type(sector_points))
-    return sector_points
+
+def save_results(raw_file, results):
+    '''
+    :param raw_file: file to be created or updated with results
+    :param results: the data to be saved to raw_file
+    :return: nothing. File is saved and closed within the function
+    '''
+    #  results_file = open(raw_file, 'w')
+    with open(raw_file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(results)
+    f.close()
+    return
+
+
+def get_coordinates(data):
+    """
+    :param data: full data on either postcode sector or speedtest result
+    :return: list of only lat and lon needed for np arrays.
+    These are in same order as the full data so that the 'winning' postcode can be found
+    """
+    coordinates = []
+    for point in data:
+            coordinates.append((float(point['Latitude']), float(point['Longitude'])))
+    return coordinates
 
 
 def find_closest_sector(location, postcodes):
     """
-    Adds the square of latitude difference to square of longitude difference and returns the index of the element with
-    the smallest total distance
+    Takes a single location and finds closest sector using postcodes np array
+    dist_2 is an array containing the square of the distances to each sector postcode
+    There is no need to take the sqrt because we only need to identify which sector is closest and therefore don't need the actual distance
+    :param postcodes: a numpy array of lat and lon of all postcode sectors
+    :return: the position of the 'winning' postcode sector in the postcodes array
+    This will be used as the lookup value in the full sector data to get the postcode
     """
-    # print('post code array: ', postcodes)
-    # print(' post codes array - location', postcodes - location)
-    # print(' distance squared:', (postcodes - location) ** 2)
     dist_2 = np.sum((postcodes - location) ** 2, axis=1)
-    # print('dist:', dist_2)
-    # print('argmin, dist:', np.argmin(dist_2), dist_2)
     print("Your points are:", postcodes[np.argmin(dist_2)], 'in position:', np.argmin(dist_2), type(np.argmin(dist_2)))
     return int(np.argmin(dist_2))
 
-def main():
-    # Call our parse function with required file an delimiter
-    sector_points_data = parse(SECTOR_FILE, ',')
-    print(sector_points_data)
-    print(type(sector_points_data))
-    print('12th item is:', sector_points_data[12])
-    print("keys are:", sector_points_data[0].keys())
-    sector_postcodes_full = [('A', 1, 1), ('B', 2, 2), ('C', 3, 3)]
-    # sector_points = get_sector_points(sector_postcodes_full, 'list')
-    sector_points = get_sector_points(sector_points_data, 'csv')
-    locations = [(57.11, -2.1), (57.1579, -2.09048), (57.13, -2.13), (3, 1)]
-    postcodes_array = np.asarray(sector_points)
+
+def get_closest_points(locations, postcodes_array, sector_points_data):
+    """
+    Loops through the speedtest results and uses find_closest_sector() to find closest sector
+    :param locations: the lat and lon of the speedtest results that need sector postcodes
+    :param postcodes_array: array of lat and lon of postcode sectors
+    :param sector_points_data:
+    :return:
+    """
     for speedtest in locations:
         closest_sector = find_closest_sector(speedtest, postcodes_array)
-        # print(closest_sector, 'Type:', type(closest_sector), 'element:', closest_sector[0], 'postcode:', sector_points[closest_sector[0]])
         print('Index from array:', type(closest_sector))
         print('Your location is in Sector:', sector_points_data[closest_sector]['Postcode'])
         print('Your location:', speedtest)
         print('Your sector location:', sector_points_data[closest_sector])
         print('NEXT')
+
+def main():
+    # Get postcode sector data from the postcode sector csv file:
+    sector_data = parse(SECTOR_FILE, ',')
+    # Get speedtest results data from the speedtest results file:
+    speedtest_results_data = parse(RESULTS_FILE, ',')
+    speedtest_results_coordinates = get_coordinates(speedtest_results_data)
+    locations = [(57.11, -2.1), (57.1579, -2.09048), (57.13, -2.13), (3, 1)]
+    start = time.time()  # todo for testing only
+    sector_coordinates = get_coordinates(sector_data)
+    sector_coordinates_array = np.asarray(sector_coordinates)
+    get_closest_points(speedtest_results_coordinates, sector_coordinates_array, sector_data)
+    end = time.time()
+    print("the code took ", start - end, "seconds")
+    print(speedtest_results_coordinates)
 
 
 if __name__ == "__main__":
